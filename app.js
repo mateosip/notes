49f1cf2bd08a7c2eff87853dc74a5d733a96b466
Mateo <33837480+mateosip@users.noreply.github.com>
2019-09-27 14:34:44 +0200

    upload

---
+++ app.js
@@ -0,0 +1,189 @@
+import yargs from 'yargs';
+import fs from 'fs';
+import uuid from 'uuid';
+
+
+const path = './notas.txt';
+
+let obj;
+
+fs.access(path, fs.F_OK, (err) =>{
+    if (err){ //Si no existe el archivo
+        fs.writeFileSync("notas.txt");//Crea uno vacio
+    }
+    const data = fs.readFileSync("notas.txt").toString();//Leo lo que contiene el fichero y lo convierto en String
+    
+    if(data !== ""){
+        obj = JSON.parse(data);//me construyo el objeto
+    }else{//Si no hay nada me creo le objeto con un array de notas pero vacío.
+        obj = {
+            notes: [
+
+            ]
+        };
+    }
+    yargs.parse();
+    fs.writeFileSync("notas.txt",JSON.stringify(obj));//Escribimos en el fichero
+})
+const add = function(arg){ //Función para añadir notas
+  const nota = {
+    uuid: uuid.v4(), //Generacion automatica de id.
+    title: arg.title, //Titulo = captura por teclado
+    body: arg.body, //Cuerpo = captura por teclado
+    author: arg.author, //Autor = captura por teclado
+  };
+
+  obj.notes.push(nota);//Introducimos nueva nota en el array
+  console.log(`Se ha añadido la nota: ${nota.title}`);
+}
+
+const list = function(){
+    obj.notes.forEach(function(note,i){ //Recorremos todo el array de notes
+        console.log(i,note.title);  //Mostramos los indices y el título de cada nota
+    })
+}
+
+const read = function(arg){
+    obj.notes.forEach(function(note,i){
+        if(i === arg.index){
+            console.log("debug");
+            console.log(note.body);
+        }
+    })
+}
+
+const remove = function(arg){
+    obj.notes.forEach(function(note,i){
+        if(i === arg.index){
+            obj.notes.splice(i,1);
+        }
+    })
+}
+//Creamos el comando add.
+
+yargs.command({
+
+  command: 'add',
+  describe: 'Anadir una nueva nota',
+  builder: {
+    title: {
+      describe: 'Titulo',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Cuerpo',
+      demandOption: true,
+      type: 'string',
+    },
+    author: {
+      describe: 'Autor',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: add,
+});
+yargs.command({
+    command: 'list',
+    describe: 'Listar todas las notas',
+    handler: list,
+});  
+
+yargs.command({
+    command: 'read',
+    describe: 'Leyendo nota',
+    builder: {
+        index: {
+            describe: "indice",
+            demandOption: true,
+            type: "int",
+        }
+    },
+    handler: read,
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Eliminando nota',
+    builder: {
+        index: {
+            describe: "indice",
+            demandOption: true,
+            type: "int",
+        }
+    },
+    handler: remove,
+})
+
+// yargs.parse();
+
+// npm i --save yargs
+
+// yargs.command({
+
+//   command: 'remove',
+
+//   describe: 'remove a note',
+
+//   handler: function() {
+
+//     console.log('Removing a note');
+
+//   },
+
+// });
+
+// // Create add command
+
+// yargs.command({
+
+//   command: 'list',
+
+//   describe: 'list existing notes',
+
+//   handler: function(argv) {
+
+//     console.log(chalk.blue(`Listing notes`));
+
+//   },
+
+// });
+
+// yargs.command({
+
+//   command: 'read',
+
+//   describe: 'read a note',
+
+//   handler: function() {
+
+//     console.log('Reading notes');
+
+//   },
+
+// });
+
+// yargs.parse();
+
+// const obj = {
+
+//   name: 'Alberto',
+
+//   friends: ['Luis', 'Jorge', 'Maria'],
+
+// };
+
+// const str = JSON.stringify(obj);
+
+// console.log(str);
+
+// const obj2 = JSON.parse(str);
+
+// console.log(obj2);
+
+// fs.writeFileSync('notes.txt', str);
+
+// const obj3 = JSON.parse(fs.readFileSync('notes.txt').toString());
+
+// console.log(obj3);
